7e2f4ddd5479f19f07d47f55148bf9ceb330cd61
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-02-26 16:35:07 -0600

    Working pub-sub eg from docs

--- services/zmq_watch_pub.mjs
+++ services/zmq_watch_pub.mjs
@@ -1,25 +1,30 @@
-import fs from 'fs';
+// import fs from 'fs';
 import zmq from 'zeromq';
 
-const file_name = process.argv[2];
+// const file_name = process.argv[2];
+
 async function init_publisher() {
   const publisher = new zmq.Publisher();
-  await publisher.bind('tcp://*:8080');
+  await publisher.bind('tcp://127.0.0.1:8080');
   console.log('[pub]: bind port 8080');
   while (true) {
-    fs.watch(file_name, async () => {
-      console.log('[pub]: file changed, sending data...');
-      await publisher.send(
-        JSON.stringify({
-          type: 'changed',
-          file: file_name,
-          timestamp: Date.now()
-        })
-      );
-      await new Promise((resolve) => {
-        setTimeout(resolve, 500);
-      });
-    });
+    console.log('Sending multipart message...');
+    await publisher.send(['foo', 'bar']);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+
+    // fs.watch(file_name, async () => {
+    //   console.log('[pub]: file changed, sending data...');
+    //   await publisher.send(
+    //     JSON.stringify({
+    //       type: 'changed',
+    //       file: file_name,
+    //       timestamp: Date.now()
+    //     })
+    //   );
+    //   await new Promise((resolve) => {
+    //     setTimeout(resolve, 500);
+    //   });
+    // });
   }
 }
 
--- services/zmq_watch_sub.mjs
+++ services/zmq_watch_sub.mjs
@@ -2,14 +2,16 @@ import zmq from 'zeromq';
 
 async function init_subscriber() {
   const subscriber = new zmq.Subscriber();
-  subscriber.connect('tcp://localhost:8080');
+  subscriber.connect('tcp://127.0.0.1:8080');
   console.log('Subscriber connected to port 8080');
-  subscriber.subscribe(''); // Subscribe to all messages
+  subscriber.subscribe('foo'); // Subscribe to all messages
+
   for await (const [topic, msg] of subscriber) {
     console.log(`Received topic: ${topic} and message: ${msg}`);
-    const message = JSON.parse(msg);
-    const date = new Date(message.timestamp);
-    console.log(`File ${message.file} changed at ${date}`);
+
+    // const message = JSON.parse(msg);
+    // const date = new Date(message.timestamp);
+    // console.log(`File ${message.file} changed at ${date}`);
   }
 }
 
