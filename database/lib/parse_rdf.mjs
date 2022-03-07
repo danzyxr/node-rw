72030fbf8f1a1c972ee794876f0e4b867a9d3373
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-07 10:26:20 -0600

    Test for book.id

--- database/lib/parse_rdf.mjs
+++ database/lib/parse_rdf.mjs
@@ -1,4 +1,8 @@
-export default function parse_rdf() {
+import * as cheerio from 'cheerio';
+
+export default function parse_rdf(rdf) {
+  const $ = cheerio.load(rdf);
   const book = {};
+  book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
   return book;
 }
