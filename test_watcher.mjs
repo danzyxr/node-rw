80245e0a3ec15791cc075dc52c84e5c372b59287
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-06 19:53:28 -0600

    Stub for mocha test watcher (esm)

---
+++ test_watcher.mjs
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+
+function get_mjs_files(directory) {
+  let files_list = [];
+  function recurse(dir) {
+    fs.readdirSync(dir).forEach((file) => {
+      const absolute_path_of_file = path.join(dir, file);
+      if (
+        dir !== 'data' &&
+        dir !== 'node_modules' &&
+        fs.statSync(absolute_path_of_file).isDirectory()
+      ) {
+        // Recursively look through folders
+        return recurse(absolute_path_of_file);
+      } else if (file.slice(file.length - 4) === '.mjs') {
+        return files_list.push(absolute_path_of_file);
+      }
+    });
+  }
+  recurse(directory);
+  return files_list;
+}
+
+let mjs = get_mjs_files('.');
+
+for (let each of mjs) {
+  console.log(each);
+}
