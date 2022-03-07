8b04a54380ccac97dc22e3d297a2ba547537b091
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-07 11:51:54 -0600

    Test for book.authors

--- database/lib/parse_rdf.mjs
+++ database/lib/parse_rdf.mjs
@@ -5,5 +5,10 @@ export default function parse_rdf(rdf) {
   const book = {};
   book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
   book.title = $('dcterms\\:title').text();
+  book.authors = $('pgterms\\:agent pgterms\\:name')
+    .toArray()
+    .map((x) => {
+      return $(x).text();
+    });
   return book;
 }
