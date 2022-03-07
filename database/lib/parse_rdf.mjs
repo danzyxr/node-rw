e0228007f2a39938b02137e20799cff7b3ce3e37
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-07 15:52:15 -0600

    Test for book.subjects

--- database/lib/parse_rdf.mjs
+++ database/lib/parse_rdf.mjs
@@ -3,6 +3,7 @@ import * as cheerio from 'cheerio';
 export default function parse_rdf(rdf) {
   const $ = cheerio.load(rdf);
   const book = {};
+
   book.id = +$('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', '');
   book.title = $('dcterms\\:title').text();
   book.authors = $('pgterms\\:agent pgterms\\:name')
@@ -10,5 +11,18 @@ export default function parse_rdf(rdf) {
     .map((x) => {
       return $(x).text();
     });
+
+  book.subjects = $("[rdf\\:resource$='/LCSH']")
+    // Subjects are stored in <rdf:Description> <rdf:value>
+    // tags, but there are 44 Description tags and 38 value
+    // tags. The tags we need are siblings to
+    // <rdf:resource>, but only if its URL ends in '/LCSH'
+    .parent()
+    .find('rdf\\:value')
+    .toArray()
+    .map((x) => {
+      return $(x).text();
+    });
+
   return book;
 }
