3514184d99481c50f495f5c782a90165d9cb431b
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-07 10:25:33 -0600

    Fix erroneous typo

--- get_gutenberg_data.sh
+++ get_gutenberg_data.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 mkdir -p data/
-wget -P data/http://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2
+wget -P data/ http://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.bz2
 # Warning: fully extracted tar has 100,000+ items, adding up to ~1GB
-tar -xvjf data/rdf-files.tar.bz2 -C data/
+# tar -xvjf data/rdf-files.tar.bz2 -C data/
