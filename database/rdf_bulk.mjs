1e51084ba816abae265cd9a38eb8805460822470
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-03-20 18:46:40 -0500

    Use node-dir to traverse data and find .rdf files

---
+++ database/rdf_bulk.mjs
@@ -0,0 +1,17 @@
+import dir from 'node-dir';
+import parse_rdf from './lib/parse_rdf.mjs';
+
+const directory = process.argv[2];
+
+const options = {
+  match: /\.rdf$/,
+  exclude: ['pg0.rdf'] // Ignore template (ID == 0)
+};
+
+dir.readFiles(directory, options, (err, content, next) => {
+  if (err) throw err;
+  const parsed_document = parse_rdf(content);
+  console.log(JSON.stringify({ index: { _id: `pg${parsed_document.id}` } }));
+  console.log(JSON.stringify(parsed_document));
+  next();
+});
