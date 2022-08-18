d50ff94959e0a01698d805309b6e03381ecae7b4
dj-mc <49037253+dj-mc@users.noreply.github.com>
2022-08-17 22:29:55 -0500

    Fix zmq reply cluster

--- services/zmq_filer_reply_cluster.mjs
+++ services/zmq_filer_reply_cluster.mjs
@@ -1,34 +1,24 @@
 import fs from 'fs';
 import cluster from 'cluster';
 import os from 'os';
-import zmq from 'zeromq';
+import zmq from '../node_modules/zeromq/v5-compat.js';
 
 const num_of_workers = os.cpus().length;
 
 if (cluster.isPrimary) {
-  const router = new zmq.Router();
-  const dealer = new zmq.Dealer();
+  const router = zmq.socket('router');
+  const dealer = zmq.socket('dealer');
 
-  async function bind_sockets() {
-    await router.bind('tcp://127.0.0.1:8080');
-    await dealer.bind('ipc://filer-dealer.ipc');
-  }
+  router.bind('tcp://127.0.0.1:8080');
+  dealer.bind('ipc://filer-dealer.ipc');
 
-  async function await_router() {
-    for await (const [msg] of router) {
-      console.log('Dealer received: ', msg.toString());
-      await dealer.send(msg);
-    }
-  }
+  router.on('message', (...msg) => {
+    dealer.send(msg);
+  });
 
-  async function await_dealer() {
-    for await (const [msg] of dealer) {
-      console.log('Router received: ', msg.toString());
-      await router.send(msg);
-    }
-  }
-
-  await bind_sockets();
+  dealer.on('message', (...msg) => {
+    router.send(msg);
+  });
 
   cluster.on('online', (worker) => {
     console.log(`Worker ${worker.process.pid} is now online`);
@@ -39,28 +29,23 @@ if (cluster.isPrimary) {
   for (let i = 0; i < num_of_workers; i++) {
     cluster.fork();
   }
-
-  await await_router();
-  await await_dealer();
 } else {
-  async function run_cluster() {
-    const responder = new zmq.Reply();
-    await responder.bind('ipc://filer-dealer.ipc');
-    for await (const [msg] of responder) {
-      console.log('Inside await of reply socket');
-      const request = JSON.parse(msg);
-      console.log(`${process.pid} requesting ${request.path}`);
-      fs.readFile(request.path, (err, data) => {
-        console.log(`Response sent from ${process.pid}`);
-        responder.send(
-          JSON.stringify({
-            content: data.toString(),
-            timestamp: Date.now(),
-            pid: process.id
-          })
-        );
-      });
-    }
-  }
-  run_cluster();
+  const responder = zmq.socket('rep');
+  responder.connect('ipc://filer-dealer.ipc');
+
+  responder.on('message', (msg) => {
+    const request = JSON.parse(msg);
+    console.log(`${process.pid} requesting ${request.path}`);
+
+    fs.readFile(request.path, (err, file_data) => {
+      console.log(`Response sent from ${process.pid}`);
+      responder.send(
+        JSON.stringify({
+          content: file_data.toString(),
+          timestamp: Date.now(),
+          pid: process.id
+        })
+      );
+    });
+  });
 }
--- services/zmq_filer_request.mjs
+++ services/zmq_filer_request.mjs
@@ -1,20 +1,19 @@
-import zmq from 'zeromq';
+import zmq from '../node_modules/zeromq/v5-compat.js';
 
 const file_name = process.argv[2];
 
-async function init_requester() {
-  const requester = new zmq.Request();
-  requester.connect('tcp://127.0.0.1:8080');
-  console.log('Requester connected to 8080');
+const requester = zmq.socket('req');
+console.log('Requester connected to 8080');
 
-  for (let i = 0; i < 5; i++) {
-    await requester.send(JSON.stringify({ path: file_name }));
-    console.log(`Awaiting reply for request: ${file_name}`);
-    const [msg] = await requester.receive();
-    console.log('Received reply:', JSON.parse(msg));
-  }
+requester.on('message', (data) => {
+  const response = JSON.parse(data);
+  console.log(response);
+});
 
-  console.log('Done!');
+requester.connect('tcp://127.0.0.1:8080');
+
+for (let i = 0; i < 5; i++) {
+  requester.send(JSON.stringify({ path: file_name }));
 }
 
-init_requester();
+console.log('Done!');
